6b4c723d6c2c073b7fd5327e43fe299f9d7cd65d
Ilya Uhlianitsa <ilja.uglyanitsa@gmail.com>
2022-10-03 11:49:36 +0400

    Graphs.DFS and DFS_Recursive

---
+++ Algorithms.Tests/GraphsTests.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Algorithms.Tests;
+
+[TestFixture]
+public class GraphsTests
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void BFS_Tests(Dictionary<string, string[]> dict, string start, bool result)
+    {
+        Graphs.BFS(dict, start).Should().Be(result);
+    }
+
+    [TestCaseSource(nameof(TestCases))]
+    public void DFS_Tests(Dictionary<string, string[]> dict, string start, bool result)
+    {
+        Graphs.DFS(dict, start).Should().Be(result);
+    }
+
+    [TestCaseSource(nameof(TestCases))]
+    public void DFS_Recursive_Tests(Dictionary<string, string[]> dict, string start, bool result)
+    {
+        Graphs.DFS_Recursive(dict, start).Should().Be(result);
+    }
+
+    private static object[] TestCases =
+    {
+        new object[]
+        {
+            new Dictionary<string, string[]>
+            {
+                { "Petr", new[] { "Luke" } },
+                { "Luke", new[] { "Petr" } }
+            },
+            "Petr", false
+        },
+        new object[]
+        {
+            new Dictionary<string, string[]>
+            {
+                { "Petr", new[] { "Luke" } },
+                { "Luke", new[] { "John" } },
+                { "John", new[] { "Adam" } }
+            },
+            "Petr", true
+        },
+        new object[]
+        {
+            new Dictionary<string, string[]>
+            {
+                { "Petr", new[] { "Luke", "John" } },
+                { "Luke", new[] { "Ivan", "Rosalie", "Bill" } },
+                { "John", new string[] { } },
+                { "Ivan", new string[] { } },
+                { "Rosalie", new[] { "John" } },
+                { "Bill", new string[] { } }
+            },
+            "Petr", false
+        },
+        new object[]
+        {
+            new Dictionary<string, string[]>
+            {
+                { "Petr", new[] { "Luke", "John" } },
+                { "Luke", new[] { "Ivan", "Rosalie", "Bill" } },
+                { "John", new string[] { } },
+                { "Ivan", new string[] { } },
+                { "Rosalie", new[] { "Adam" } },
+                { "Bill", new string[] { } }
+            },
+            "Petr", true
+        },
+        new object[] { new Dictionary<string, string[]> { { "Petr", new[] { "Adam" } } }, "Petr", true },
+    };
+}
---
+++ Algorithms/Graphs.cs
@@ -0,0 +1,68 @@
+﻿namespace Algorithms;
+
+public static class Graphs
+{
+    public static bool BFS(Dictionary<string, string[]> dict, string start)
+    {
+        var queue = new Queue<string>();
+        var selected = new HashSet<string>();
+        dict[start].ToList().ForEach(x => queue.Enqueue(x));
+
+        while (queue.TryDequeue(out var key))
+        {
+            if (key.EndsWith("m"))
+                return true;
+            
+            if(!selected.Add(key))
+                continue;
+
+            dict[key].ToList().ForEach(x => queue.Enqueue(x));
+        }
+
+        return false;
+    }
+    
+    public static bool DFS(Dictionary<string, string[]> dict, string start)
+    {
+        var stack = new Stack<string>();
+        var selected = new HashSet<string>();
+        dict[start].ToList().ForEach(x => stack.Push(x));
+
+        while (stack.TryPop(out var key))
+        {
+            if (key.EndsWith("m"))
+                return true;
+            
+            if(!selected.Add(key))
+                continue;
+            
+            dict[key].ToList().ForEach(x => stack.Push(x));
+        }
+        
+        return false;
+    }
+    
+    public static bool DFS_Recursive(Dictionary<string, string[]> dict, string start)
+    {
+        var selected = new HashSet<string>();
+
+        return Internal(start);
+        
+        bool Internal(string key)
+        {
+            if (key.EndsWith("m"))
+                return true;
+
+            var result = false;
+            foreach (var item in dict[key])
+            {
+                if(!selected.Add(item))
+                    continue;
+                
+                result |= Internal(item);
+            }
+
+            return result;
+        }
+    }
+}
